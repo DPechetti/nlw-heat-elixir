62ebcae99784a2a745fa11a5c0e9fa21cb05db82
Donovan Pechetti <dovi_pf@hotmail.com>
2021-10-30 07:12:02 -0300

    message schema created

---
+++ lib/heat_tags/message.ex
@@ -0,0 +1,23 @@
+defmodule HeatTags.Message do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @requiredParams [:message, :username, :email]
+
+  schema "messages" do
+    field :message, :string
+    field :username, :string
+    field :email, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @requiredParams)
+    |> validate_required(@requiredParams)
+    |> validate_length(:message, min: 1, max: 140)
+    |> validate_format(:email, ~r/@/)
+  end
+end
